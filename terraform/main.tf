770bc740bd2f26dc9316d467054d4d7972df0595
Adam Coales <coalez@googlemail.com>
2018-03-15 21:29:42 +0000

    Further playing with terraform to get image onto ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+variable "login" {}
+variable "image" {}
+variable "key" {}
+
 provider "aws" {
   region = "eu-west-1"
 }
@@ -5,12 +9,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-a61464df"
   instance_type = "t2.micro"
+  key_name = "userapi"
 
   tags {
     Name = "terraform-example"
   }
 
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  connection {
+    user = "core"
+    private_key = "${file("${var.key}")}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "${var.login}",
+      "docker pull ${var.image}",
+      "docker run --rm -d -p8080:8080 4ce757029c74"
+    ]
+  }
 }
 
 resource "aws_security_group" "instance" {
@@ -21,6 +39,19 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+ 
 }
 
 output "public_ip" {
